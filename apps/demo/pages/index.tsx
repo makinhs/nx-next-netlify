1d19604c5c0f0d88cc43bc84c71d8a1840707cfa
Marcos Silva <tio.makin@gmail.com>
2022-07-14 16:12:05 +0200

    update: welcome netlify

--- apps/demo/pages/index.tsx
+++ apps/demo/pages/index.tsx
@@ -18,7 +18,7 @@ export function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome demo 👋
+              Welcome netlify
             </h1>
           </div>
 
